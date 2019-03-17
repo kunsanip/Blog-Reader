858a16029c0e891eec3b200d56339f31c401a853
Sanip Shrestha <sanip_7@live.com>
2019-03-17 13:04:24 +1100

    * display title instead of timestamp * save in coredata successfully

--- Blog Reader/MasterViewController.swift
+++ Blog Reader/MasterViewController.swift
@@ -42,13 +42,38 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
                      
                         if let items = jsonResult["items"] as? NSArray
                         {
+                            let context = self.fetchedResultsController.managedObjectContext
+                            
+                            let request = NSFetchRequest<Event>(entityName: "Event")
+                            
+                            do{
+                                let results = try context.fetch(request)
+                                
+                                if results.count > 0 {
+                                    
+                                    for result in results{
+                                        context.delete(result)
+                                        do{
+                                            try context.save()
+                                            
+                                        }catch{
+                                            print("Specific delete failed.")
+                                        }
+                                    }
+                                }
+                                
+                            }
+                            catch{
+                                print("Delete Failed.")
+                            }
+                            
                             var i = 1
                             for item in items as [AnyObject]{
                                 
                                 print("Item \(i): ")
                                 i += 1
                                 
-                                let context = self.fetchedResultsController.managedObjectContext
+                               
                                 let newEvent = Event(context: context)
                                 
                                 // If appropriate, configure the new managed object.
@@ -164,7 +189,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     }
 
     func configureCell(_ cell: UITableViewCell, withEvent event: Event) {
-        cell.textLabel!.text = event.timestamp!.description
+        cell.textLabel!.text = event.value(forKey: "title") as? String
     }
 
     // MARK: - Fetched results controller
